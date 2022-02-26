6765fd0847d422fad7199243404b5eba03b9367d
Dominik Smida <dodalovilp@seznam.cz>
2022-02-26 09:46:06 +0100

    Project without function main

--- main.py
+++ main.py
@@ -3,171 +3,156 @@ import requests
 import csv
 import pandas as pd
 from pprint import pprint
+import sys
+o = '====='*5
 
+def main():
 
-url = "https://volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=12&xnumnuts=7103"
+#---------------------------------LET'S FIND ALL TABLES----------------------------------------
+    url = sys.argv[1]
+    get_req = requests.get(url)
+    soup = BeautifulSoup(get_req.text, 'html.parser')
+    print(o)
+    print(f'Loading the URL...')
+    table = soup.findAll('table')
 
-get_req = requests.get(url)
-soup = BeautifulSoup(get_req.text, 'html.parser')
+    list_radku = []
+    list_odkazu = []
 
-table = soup.findAll('table')
-
-list_radku = []
-list_odkazu = []
-
-#funkce pro zápis do CSV
-def csv_file_writer(filename):
-#----------------------------HEADER OF CSV----------------------------------
-
-#----------------------------OPENING AND WRITING INTO CSV-------------------
-
-    f = open(filename,'w',newline='', encoding='utf-8')
-    headers = ["code", "location", "registered", "envelopes", "valid"]
-    #---------write all strany to the header---------
-    for index, strana in enumerate(jme):
-        headers.append(jme[index])
-    write = csv.writer(f, delimiter=';', quotechar='"')
-    write.writerow(headers)
-    write.writerows(list_radku)
-    print('done')
-    f.close()
-
-
-#now check and delete empty rows
-    df = pd.read_csv(filename, sep='\t')
-# Droping the empty rows
-    modifiedDF = df.dropna()
-# Saving it to the csv file
-    modifiedDF.to_csv(filename, index=False)
-
-#-----------------------------LET'S FIND ALL TABLES-------------------------
-for tabulka in table:
-
-    for row in tabulka.findAll('tr'):
-
-        list_bunek = []
-        odkazy = []
-
-        for cell in row.findAll(['td']):
-
-            for a in row.findAll('a',href=True):
-                #if a.text:
-                    odkazy.append(a["href"])
-
-            if cell not in row.findAll('td',{"class":"hidden_td"}):
-
-                text = cell.get_text()
-
-                if text != 'X':     #zahození znaku X pro sběr dat
-                    list_bunek.append(text)
-        list_odkazu.append(odkazy)
-        list_radku.append(list_bunek)
-list_odkazu_o = []
-
-#ziska odkazy z tabulky
-for i in list_odkazu:
-    if i != []:
-        #print(i)
-        odkaz = i
-        list_odkazu_o.append(odkaz[0])
-
-#ukončí GET první URL
-soup.clear()
-#-----------------------------------pošle GET na další URL----------------------------------------
-tab_1 = []
-tab_2 = []
-detailni = []
-#-----------------------------------loop for all found urls----------------------------
-for i in list_odkazu_o:
-    url_1 = "https://volby.cz/pls/ps2017nss/"+i
-    get_req_o = requests.get(url_1)
-    soup_o = BeautifulSoup(get_req_o.text, 'html.parser')
-    #print(soup_o)
-    table_o = soup_o.findAll('table')
-    table_jmena = soup_o.find('table')
-    jme=[]
-
-
-    for tabulka in table_o:
+    for tabulka in table:
 
         for row in tabulka.findAll('tr'):
-            udaje = []
 
-#-----------------------------------vytáhne údaje z 1. tabulky  pro všechny okrsky-----------------
+            list_bunek = []
+            odkazy = []
 
-            for cell in tabulka.findAll('td', {'class': 'cislo'}):
+            for cell in row.findAll(['td']):
+
+                for a in row.findAll('a',href=True):
+                    odkazy.append(a["href"])
+
+                if cell not in row.findAll('td',{"class":"hidden_td"}):
+
+                    text = cell.get_text()
+
+                    if text != 'X':     #zahození znaku X pro sběr dat
+                        list_bunek.append(text)
+            list_odkazu.append(odkazy)
+            list_radku.append(list_bunek)
+    list_odkazu_o = []
+
+#ziska odkazy z tabulky
+    for i in list_odkazu:
+        if i != []:
+            odkaz = i
+            list_odkazu_o.append(odkaz[0])
+
+#-----------------------------------End the first URL-----------------------------------------------
+    soup.clear()
+
+#-----------------------------------Sends get for another URL----------------------------------------
+    print('Loading the data...')
+    tab_1 = []
+    for i in list_odkazu_o:
+        url_1 = "https://volby.cz/pls/ps2017nss/"+i
+        get_req_o = requests.get(url_1)
+        soup_o = BeautifulSoup(get_req_o.text, 'html.parser')
+        table_o = soup_o.findAll('table')
+        jme=[]
+
+#------------------------------------for x table in table_o--------------------------
+        for tabulka in table_o:
+
+            for row in tabulka.findAll('tr'):
+                udaje = []
+#-----------------------------------Take data from first table for all towns-----------------
+                for cell in tabulka.findAll('td', {'class': 'cislo'}):
 
                     text = cell.get_text()
                     udaje.append(text)
 
-        tab_1.append(udaje)  # každou tabulku zapíše do listu
-#        continue
-
-jmena_stran = []
-#------------------------------------uloží jména kandidujících stran do jednoho listu---------------
-jme=[]
-for cell in soup_o.findAll('td',{'class':'overflow_name'}):
+            tab_1.append(udaje)
+#------------------------------------Saves the names of candidates into one list---------------
+    jme=[]
+    for cell in soup_o.findAll('td',{'class':'overflow_name'}):
         text = cell.get_text()
         jme.append(text)
-print(jme)
 
-#------------------------------------vytáhne data z první tabulky-----------------------------------
-prvni_tabulka = tab_1[::3]
-new_tab = []
-prvni_souhrn = []
-#------------------------------------upraví data z první tabulky potřebné pro export----------------
-for i, a in enumerate(prvni_tabulka):
-    prvni = []
-    for cislo in a:
-        if cislo == a[3]:
-            prvni.append(cislo)
+#------------------------------------Take data from first table and save it into the variable----------------
+    prvni_tabulka = tab_1[::3]
+    prvni_souhrn = []
+    for i, a in enumerate(prvni_tabulka):
+        prvni = []
+        for cislo in a:
+            if cislo == a[3]:
+                prvni.append(cislo)
 
-        elif cislo == a[4]:
-            prvni.append(cislo)
-            break
-    for cislo in a:
-        if cislo == a[7]:
-            prvni.append(cislo)
-            break
+            elif cislo == a[4]:
+                prvni.append(cislo)
+                break
 
-    prvni_souhrn.append(prvni)
-#------------------------------------vytáhne data z druhé tabulky-----------------------------------
-druha_tabulka = tab_1[1::3]
-data_druha = []
-for a in druha_tabulka:
-    data=[]
-    data = a[1::3]
-    data_druha.append(data)
-#------------------------------------vytáhne data z třetí tabulky-----------------------------------
-treti_tabulka = tab_1[2::3]
-data_treti = []
-for a in treti_tabulka:
-    data=[]
-    data = a[1::3]
-    data_treti.append(data)
+        for cislo in a:
+            if cislo == a[7]:
+                prvni.append(cislo)
+                break
 
-#----------------------------------------Collects data for each town------------------------------------
-for i, a in enumerate(tab_1):
-    if i in range(0,len(tab_1), 3):
-        new_tab.append(tab_1[i:i+3])
+        prvni_souhrn.append(prvni)
+#------------------------------------Take data from second table and save it into the variable----------------
+    druha_tabulka = tab_1[1::3]
+    data_druha = []
+    for a in druha_tabulka:
+        data=[]
+        data = a[1::3]
+        data_druha.append(data)
+#------------------------------------Take data from third table and save it into the variable----------------
+    treti_tabulka = tab_1[2::3]
+    data_treti = []
+    for a in treti_tabulka:
+        data=[]
+        data = a[1::3]
+        data_treti.append(data)
 
 #----------------------------------------Remove blank lists---------------------------------------------
-list_radku_f = [x for x in list_radku if x]
-#----------------------------------------writing data into final list before exporting to csv-----------
+    list_radku_f = [x for x in list_radku if x]
 
-for i, mesto in enumerate(list_radku_f):
-    if i in range(0, len(list_radku_f)):
-        list_radku_f[i].append(prvni_souhrn[i][0]) #zapíše hodnotu z 1.tabulky
-        list_radku_f[i].append(prvni_souhrn[i][1]) #zapíše hodnotu z 1. tabulky
-        list_radku_f[i].append(prvni_souhrn[i][2])  # zapíše hodnotu z 1. tabulky
-        for index, data in enumerate(data_druha[i]):
-            list_radku_f[i].append(data_druha[i][index]) #zapíše hodnotu z 2.tabulky
-        for index, data in enumerate(data_treti[i]):
-            list_radku_f[i].append(data_treti[i][index])  # zapíše hodnotu z 3. tabulky
+#----------------------------------------Writing data into final list before exporting to csv-----------
+    for i, mesto in enumerate(list_radku_f):
+        if i in range(0, len(list_radku_f)):
+            list_radku_f[i].append(prvni_souhrn[i][0]) #write a value from first table
+            list_radku_f[i].append(prvni_souhrn[i][1])
+            list_radku_f[i].append(prvni_souhrn[i][2])
+            for index, data in enumerate(data_druha[i]): #write a value from second table
+                list_radku_f[i].append(data_druha[i][index])
+            for index, data in enumerate(data_treti[i]): #write a value from third table
+                list_radku_f[i].append(data_treti[i][index])
 
 #-----------------------------------------check and remove \xa0 characters-------------------------
-    for temp_index,a in enumerate(mesto):
-        if '\xa0' in a:
-            list_radku_f[i][temp_index] = a.replace(u'\xa0',u'')
+        for temp_index,a in enumerate(mesto):
+            if '\xa0' in a:
+                list_radku_f[i][temp_index] = a.replace(u'\xa0',u'')
 
-csv_file_writer('ahojda10.csv')
+#----------------------------OPENING AND WRITING INTO CSV-------------------
+    print("Writing the data into CSV...")
+    f = open(sys.argv[2],'w',newline='', encoding='utf-8')
+    headers = ["code", "location", "registered", "envelopes", "valid"]
+
+    #---------Write all names of candidates to the header---------
+    for index, strana in enumerate(jme):
+        headers.append(strana.replace(',','|'))
+    write = csv.writer(f, delimiter=';')
+    write.writerow(headers)
+    write.writerows(list_radku)
+    print(f'The export to "{sys.argv[2]}.csv" has been done.')
+    print(o)
+    f.close()
+
+
+#now check and delete empty rows
+    df = pd.read_csv(sys.argv[2], sep='\t')
+# Droping the empty rows
+    modifiedDF = df.dropna()
+# Saving it to the csv file
+    modifiedDF.to_csv(sys.argv[2], index=False)
+
+if __name__ == '__main__':
+    main()
